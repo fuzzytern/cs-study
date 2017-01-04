3d800ecb6c84a0d87ee37a262c8686adc37ceda0
Nicolas Wormser <nicolas@cybercrumbs.net>
2017-01-04 16:08:51 +0400

    Add basic linked list  implementation

--- linkedList.c
+++ linkedList.c
@@ -1,1 +1,21 @@
+struct node {
+  int data;
+  node* next;
+};
+typedef struct node node;
 
+node* link(int data, node* next){  
+  node list = { data, next };
+  return &list;
+};
+node prepend(node* list, node* head){  
+  head->next = list;
+  list = head;
+};
+int length(node *list){
+};
+void append(){
+};
+int main(void){
+  node* list = link(1,link(2,link(3,link(4,link(5,NULL)))));
+};  
