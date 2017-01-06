237eed49700b2b256b3f0b9f5572f52547fc6b64
Nicolas Wormser <nicolas@cybercrumbs.net>
2017-01-06 09:45:13 +0400

    Implement add and find for binary search trees

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -1,1 +1,33 @@
+struct bst {
+  int value;
+  bst* left;
+  bst* right;
+};
+typedef struct bst bst;
 
+bst initBst(int val){
+  bst tree = { val, NULL, NULL };
+  return tree;
+};
+void add(int val, bst* tree){
+  if(!tree){
+    bst t = initBst(val);
+    tree = &t;
+  }
+  else if(val < tree->value)
+    add(val, tree->left);
+  else
+    add(val, tree->right);
+};
+
+bst* find(int val, bst* tree){
+  if(!tree) return NULL;
+  if(val == tree->value) return tree;
+  else if(val < tree->value)
+    find(val, tree->left);
+  else // val > tree->value
+    find(val, tree->right);
+};
+
+
+    
