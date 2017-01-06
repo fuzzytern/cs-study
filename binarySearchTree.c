9b3896bceeb16c8bcbc4e0c5b0d2df04b8d2e644
Nicolas Wormser <nicolas@cybercrumbs.net>
2017-01-06 11:09:30 +0400

    Add remove and some tests for binary search trees

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 struct bst {
   int value;
   bst* left;
@@ -8,7 +10,7 @@ typedef struct bst bst;
 bst initBst(int val){
   bst tree = { val, NULL, NULL };
   return tree;
-};
+}
 void add(int val, bst* tree){
   if(!tree){
     bst t = initBst(val);
@@ -18,8 +20,7 @@ void add(int val, bst* tree){
     add(val, tree->left);
   else
     add(val, tree->right);
-};
-
+}
 bst* find(int val, bst* tree){
   if(!tree) return NULL;
   if(val == tree->value) return tree;
@@ -27,7 +28,37 @@ bst* find(int val, bst* tree){
     find(val, tree->left);
   else // val > tree->value
     find(val, tree->right);
-};
-
-
+}
+void remove(int val, bst* tree){
+  bst* node = find(val, tree);
+  if(!node) return;
+  if(node->left && !node->right)
+    node = node->left;
+  else if(node->right && !node->left)
+    node = node->right;
+  else { // node->left && node->right
+    bst* smallest = smallestChild(node->right);
+    node->value = smallest->value;
+    remove(smallest->value, smallest);
+  }
+}
+bst* smallestChild(bst* node){
+  if(!node->left) return node;
+  else
+    smallestChild(node->left);
+}
+int main(void){
+  bst* tree = initBst(5);
+  add(2,tree);
+  add(7,tree);
+  add(-2,tree);
+  add(3,tree);
+  add(19,tree);
+  add(10,tree);
+  remove(10,tree);
+  bst* leaf = find(19, tree);
+  assert(!leaf->left && !leaf->right);
+  remove(2,tree);
+  assert(tree->value == 3);
+}
     
