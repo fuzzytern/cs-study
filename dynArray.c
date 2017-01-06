d8a887cf15f284217290e1a0ccfbab8f1692d01b
Nicolas Wormser <nicolas@cybercrumbs.net>
2017-01-06 11:12:11 +0400

    Remove extra semicolons

--- dynArray.c
+++ dynArray.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>; // for size_t
-#include <stdio.h>; // for printf
+#include <stdlib.h> // for size_t
+#include <stdio.h> // for printf
 
 struct dynArray {
   size_t size;
@@ -12,26 +12,22 @@ dynArray createDynArray(int *d, size_t s){
   dynArray dArr = { s, 0, d };
   extend(&dArr);
   return dArr;
-};
-
+}
 char set(dynArray dArr, int it, size_t idx){
   if(idx > capacity)
     return -1; // Used for handling errors
   dArr.data[idx] = it;
   return 0;
-};
-
+}
 int get(dynArray dArr, size_t idx){
   if(dArr.capacity == 0 || idx > dArr.capacity)
    retdArr.data[idx];
-};
-
+}
 void push(dynArray *dArr, int it){
   extend(dArr);
   dArr->data[dArray->size] = it;
   dArr->size += 1;
-};
-
+}
 void extend(dynArray *dArr){
   if(dArr->capacity <= dArr->size){
     dArr->capacity = dArr->size*2; // Just doubling the capacity
@@ -40,8 +36,7 @@ void extend(dynArray *dArr){
       arr[i] = dArr->data[i];
     dArr->data = arr;
   };
-};
-
+}
 int main(void){
   int[] items = { 1,2,0,4 };
   dynArray dA = createDynArray(items, sizeof items / sizeof *items);
@@ -49,4 +44,4 @@ int main(void){
   push(&dA, 5); // Adds 5 to the end of the array.
   for(int i=0; i < dA.size; i++) // should print 1 2 3 4 5
     printf("%d ", get(dA, i));
-};
+}
