876fdcb01184b07818bfe2d7132b1c04f96a972d
Nicolas Wormser <nicolas@cybercrumbs.net>
2017-01-06 11:10:59 +0400

    Remove extra semicolons

--- linkedList.c
+++ linkedList.c
@@ -1,4 +1,4 @@
-#include <assert.h>;
+#include <assert.h>
 
 struct node {
   int data;
@@ -11,8 +11,7 @@ typedef struct node node;
 node* link(int data, node* next){  
   node list = { data, next };
   return &list;
-};
-
+}
 // Puts node elem at the front of the list
 void prepend(node* list, node* elem){  
   elem->next = list;
@@ -24,8 +23,7 @@ int length(node* list){
   if(list == NULL)
     return 0;
   return 1 + length(list->next);
-};
-
+}
 // Adds elem as the last node of the list
 void append(node* list, node* elem){
   if(list == NULL) return;
@@ -33,11 +31,10 @@ void append(node* list, node* elem){
     append(list->next, elem);
   else
     list->next = elem;
-};
-
+}
 int main(void){
   node* list = link(1,link(2,link(3,link(4,link(5,NULL)))));
   prepend(list, 0);
   append(list, 6);
   assert(length(list) == 7); // The list should now have 7 elements
-};
+}
