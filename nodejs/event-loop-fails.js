c03adc904c3e0f4b42acd6b07a4388bd0dfea27b
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-01-18 20:46:21 +0400

    Update breaking the event loop example

--- nodejs/event-loop-fails.js
+++ nodejs/event-loop-fails.js
@@ -1,35 +1,33 @@
+// Say we want to write code that prints out "Not ready" every half second and "Finally Ready" after five second.
+// A naive implementation that ignore the asynchronous nature of setTimeout and the NodeJS event Loop could look like this:
 const EventEmitter = require('events').EventEmitter;
 const util = require('util');
+const ee = new EventEmitter;
 
-class ProgressBar extends EventEmitter {
-  constructor(){
-    super();
-    this.interrupted = false;
-  }
+// Let a variable determine indicate whether the timeout is reached
+let ready = false;
 
-  updateBar(){
-    // Writes one sign '=' to the standard output 
-    process.stdout.write('='); 
-  }
+// After 5 seconds, raise an event that will set that variable to true
+setTimeout(() => ee.emit('ready'), 5000);
+ee.on('ready', () => {
+  ready = true;
+});
 
-  interrupt(){
-    // Sets the value of `interrupt` to `true`
-    this.emit('interrupt');
-    this.interrupted = true;
-  }
-
-  run(){
-    do{
-      setTimeout(this.updateBar,1000);
-    } while(!this.interrupted); 
-  }
+// while the variable is not set to true, write "Not ready to the output"
+while (!ready){ // /!\ This is an infinite loop blocking the Node Event Loop!!!
+  setTimeout(console.log,500,"Not ready"); // Also, using setTimeout like so ignores the fact it is asynchronous
 }
+console.log("Finally Ready"); // This will never be displayed, not even after the timeout
 
-let bar = new ProgressBar;
+// Instead of line 17 to 20, the following code will not block the event loop. Note the use of recursion
+//const printReady = () => {
+//  if (!ready)
+//    setTimeout( () => {
+//        console.log("Not ready");
+//        printReady();
+//    }, 500);
+//  else
+//    console.log("Finally Ready");
+//}
+//printReady();
 
-bar.on('interrupt', ()=>{
-  console.log("Process interrupted");
-}); 
-bar.interrupt();
-// setTimeout(bar.interrupt, 2000); // FIXME figure out why it crashes: `this.emit` is not a function
-// bar.run(); // FIXME the code without this line should work return "Process interrupted"...
